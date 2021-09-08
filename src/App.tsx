3da848e3f9a2d372d22fa1223df4858ac8e0aa33
Eugene <eugene.kotsarev@gmail.com>
2021-09-09 01:57:53 +0300

    add: pretty imports (paths), but it fails to compile :(

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react'
 // import logo from './logo.svg';
-import "./App.css";
-import Footer from "./shared/footer/Footer";
+import './App.css'
+import Footer from './shared/footer/Footer'
 
-function App() {
-  return (
-    <div className="bg-blue-50 container">
-      <Footer />
-    </div>
-  );
+function App(): JSX.Element {
+	return (
+		<div className='bg-blue-50 container'>
+			<Footer />
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/shared/footer/Footer.tsx
+++ src/shared/footer/Footer.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import brand from '../../assets/images/brand/brand.svg'
 
 function Footer(): JSX.Element {
-	return <div className='bg-blue-50 container'>Footer is working</div>
+	return (
+		<div className='bg-red-50 container'>
+			Footer is working
+			<img src={brand} alt='Alt' />
+		</div>
+	)
 }
 
 export default Footer
