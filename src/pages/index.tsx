543446c6b681767ecb9dede8a6191da1d76b144d
ereburg <gimmethenick@gmail.com>
2021-09-10 15:19:43 +0300

    upd eslint config with airbnb style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
-export default function Home() {
+export default function Home(): JSX.Element {
 	return (
 		<div className="container flex items-center p-4 mx-auto min-h-screen justify-center">
 			<main>
 				<h1 className="font-mono text-xl code">
-					Welcome to{" "}
+					Welcome to{` `}
 					<span className="text-purple-700 hover:text-blue-400">Nextjs</span>,
 					<span className="text-indigo-700 hover:text-blue-400">
 						TailwindCSS
-					</span>{" "}
-					and{" "}
+					</span>
+					{` `}and{` `}
 					<span className="text-blue-700 hover:text-red-400">TypeScript</span>
 				</h1>
 			</main>
