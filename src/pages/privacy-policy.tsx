9c983c50c46603d31f01dd1d4c27322919baee28
ereburg <gimmethenick@gmail.com>
2021-09-12 18:55:23 +0300

    add footer-bottom

--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -1,12 +1,12 @@
-import Footer from "@/components/global/footer/Footer"
 import Header from "@/components/global/header/Header"
+import Footer from "@/components/global/footer/Footer"
 
-export default function Home(): JSX.Element {
+export default function PrivacyPolicy(): JSX.Element {
   return (
     <div className="flex flex-col min-h-screen font-body">
       <Header />
       <main className="flex-auto">
-        <h1 className="font-mono text-xl code">Pureidea title</h1>
+        <h1 className="font-mono text-xl code">PrivacyPolicy</h1>
       </main>
       <Footer />
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,49 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const colors = require("tailwindcss/colors")
+
 module.exports = {
-	mode: "jit",
-	purge: [
-		"./src/components/**/*.{ts,tsx,js,jsx}",
-		"./src/pages/**/*.{ts,tsx,js,jsx}",
-	],
-	darkMode: "class", // or 'media' or 'class'
-	theme: {
-		extend: {},
-	},
-	variants: {},
-	plugins: [],
+  mode: "jit",
+  purge: [
+    "./src/components/**/*.{ts,tsx,js,jsx}",
+    "./src/pages/**/*.{ts,tsx,js,jsx}",
+  ],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['"Fira Sans Condensed"', "sans-serif"],
+      },
+      colors: {
+        grey: {
+          DEFAULT: "#333333",
+          2: "#4F4F4F",
+          3: "#828282",
+          4: "#BDBDBD",
+          5: "#E0E0E0",
+          6: "#F2F2F2",
+        },
+        text: {
+          DEFAULT: "#141926",
+        },
+      },
+    },
+    container: {
+      padding: "1rem",
+      center: true,
+    },
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+      xl: "1332px",
+      // => @media (min-width: 1280px) { ... }
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+  },
+  variants: {},
+  plugins: [],
 }
