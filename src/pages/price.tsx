e056845b6c6d19183d536daf8bc7124db8943d5d
ereburg <gimmethenick@gmail.com>
2021-09-14 23:15:53 +0300

    improve header states

---
+++ src/pages/price.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/global/header/Header"
+import Footer from "@/components/global/footer/Footer"
+
+export default function Price(): JSX.Element {
+  return (
+    <div className="flex flex-col min-h-screen font-body">
+      <Header type="default" />
+      <main className="flex-auto">
+        <h1 className="font-mono text-xl code">PrivacyPolicy</h1>
+      </main>
+      <Footer />
+    </div>
+  )
+}
